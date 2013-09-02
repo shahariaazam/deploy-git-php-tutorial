fd41f268a0f893d6f535c49bb659d32e58b15128
Ubuntu <ubuntu@ip-172-31-42-114.us-west-2.compute.internal>
2013-09-03 01:59:47 +0600

    pull.php file write to update the local repo

---
+++ pull.php
@@ -0,0 +1,7 @@
+<?php
+	/**	
+	*	Lots of logic before running the exec() like verify the request source is from github or 
+	*	not or make some secret key or anything for your security
+	*/
+	
+	exec('git pull');
